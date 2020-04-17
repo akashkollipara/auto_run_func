582876fe5af72ef843113d299832d81da4e77c88
Akash Kollipara <akashkollipara@gmail.com>
2020-04-17 17:01:09 +0530

    bug_fix

--- src/auto_run.c
+++ src/auto_run.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <auto_run.h>
 
-extern func_t fa, fd;
+extern func_t _table_start, _table_end;
 
 void auto_run()
 {
 	printf("Auto run ...\n");
-	const func_t *fptr = &fa;
-	while(fptr <= &fd)
+	const func_t *fptr = &_table_start;
+	while(fptr < &_table_end)
 	{
 		printf("Running %s: ", fptr->name);
 		fptr->ops->fn();
